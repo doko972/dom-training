e55dcfde3206f72fc4079bc4a81ec322356ed9a3
Guillaume Belleuvre <guillaumebelleuvre@gmail.com>
2024-04-16 14:42:42 +0200

    question4

--- js/training.js
+++ js/training.js
@@ -19,13 +19,39 @@ window.addEventListener('scroll', function () {
 it puts the first child at the end of the list.  */
 
 document.querySelector("#ex3 .js-btn-animal")
-    .addEventListener("click", function (event) {
+    .addEventListener("click", function () {
         document.querySelector("#ex3-animals")
             .append(document.querySelector("#ex3-animals li:first-child"));
     });
 
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
+let i = 0;
+const arrayColors = [
+    "blue",
+    "red",
+    "green",
+]
+const btnEx4 = document.querySelector(".btnex4");
+btnEx4.addEventListener("click", function(event) {
+    btnEx4.classList.add(arrayColors[i % arrayColors.length]);
+    btnEx4.classList.remove(arrayColors[(i - 1) % arrayColors.length]);
+
+    // if (arrayColors[i] === 0) {
+    //     btnEx4.classList.remove("green")
+    // }
+    // if (count === 1) {
+    //     btnEx4.classList.replace("blue", "red");
+        
+    // }
+
+    // if (count === 2) {
+    //     btnEx4.classList.replace("red", "green");
+    //     count = -1;
+
+    // }
+    i++;
+})
 
 
 /* ------------------------------------ */
